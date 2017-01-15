b22bc0e6beeaffcbc2c2a86f78b60c1d66d1eaeb
Micheal Harker <micheal@michealharker.com>
2017-01-15 23:14:45 +0000

    current commit with join messages and /rules

---
+++ rules.md
@@ -0,0 +1,10 @@
+» This is a *safe-for-work* chat (we have #nsfw for porn and stuff)
+» *Only speak in English*
+» *No spam* (this includes advertising, repeating things over and over again, or spamming bot commands)
+» *Don't flood the chat*, it will result in a kick
+» *Please be inclusive.* This doesn't mean you can't express your opinion, but don't be an asshole about it
+» *Please try our games*, but don't cheat - it ruins the experience for everybody
+
+[#lounge](https://t.me/lounge6697)
+[#lounge-de](https://t.me/joinchat/BrliRj6tra4BssLoLW9qgA)
+[#nsfw](https://t.me/nsfw6697)
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import fs from 'fs'
+import path from 'path'
+
 import dude from 'debug-dude'
 const { /*debug,*/ log, info, warn /*, error*/ } = dude('bot')
 
@@ -9,8 +12,6 @@ import config from '../config.json'
 import { connect } from 'coffea'
 const networks = connect(config)
 
-// --
-
 networks.on('connection', ({ network, ...options }) =>
   info('connected to %s network: %o', network, options)
 )
@@ -19,21 +20,32 @@ networks.on('error', ({ err }) =>
   warn('network error: %o', err)
 )
 
-networks.on('message', (evt, reply) => {
+networks.on('new_chat_participant', (evt, reply) => {
   log('Received message event: %o', evt)
-
-  // TODO: do something with messages here or remove the message event handler
+  let name = evt && evt.raw && evt.raw.new_chat_participant && [ evt.raw.new_chat_participant.first_name, evt.raw.new_chat_participant.last_name ].join(' ')
+  reply({
+    type: 'message',
+    text: `Welcome to #lounge, *${name}*. Please read the rules (/rules) and enjoy your time here!`,
+    options: {
+      parse_mode: 'markdown',
+      reply_to_message_id: evt && evt.raw && evt.raw.message_id
+    }
+  })
 })
 
 networks.on('command', (evt, reply) => {
   log('Received command event: %o', evt)
 
   switch (evt.cmd) {
-    case 'say':
-      reply(evt.args.join(' '))
+    case 'rules':
+      reply({
+        type: 'message',
+        text: fs.readFileSync(path.join(__dirname, '..', 'rules.md')),
+        options: {
+          parse_mode: 'markdown',
+          disable_web_page_preview: true
+        }
+      })
       break
-    // TODO: add more commands here
   }
 })
-
-// TODO: write more code here or adjust code above
